99631b6b2618c5da62b5dfad8cfc0adb3b48ce25
Atyant Sony <atyant20039@iiitd.ac.in>
2021-11-05 23:47:54 +0530

    Added speech recognition for input string and text-to-speech for output string

--- text_translate.py
+++ text_translate.py
@@ -6,14 +6,53 @@
 # Learn more about translate here:-
 # https://pypi.org/project/translate/
 
-from translate import Translator
+# Update 1:
+# Added functionality to speak out the translated output.
+# I have used gtts library for producing the audio and playsound library to play the produced audio
 
-print("Enter String to translate:")
-in_text = input()
+# Update 2:
+# Added speech recognition to the program so that it can take input string using speech recognition. The user still needs to type the output language.
+# I have used speech_recognition, pyaudio & pyspeech libraries for the above purpose.
+
+
+from translate import Translator
+import speech_recognition as sr
+from playsound import playsound
+from gtts import gTTS
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+    print("Processing...")
+try:
+    in_text = r.recognize_google(audio)
+    print("Input String: " + in_text)
+except sr.UnknownValueError:
+    print("Sorry! Unable to understand you.")
+    print("Try typing in the input")
+    in_text = input()
+except sr.RequestError:
+    print("Unable to request Google Speech Recognition services. Make sure you have good internet connection.")
+    print("Try typing in the input")
+    in_text = input()
 
 print("Enter Output Language:")
 out_lang = input()
 
-translator = Translator(to_lang = out_lang)
-trans_text = translator.translate(in_text)
-print(trans_text)
+try:
+    translator = Translator(to_lang = out_lang)
+    trans_text = translator.translate(in_text)
+    print(trans_text)
+except:
+    print("Error while translating. Probably the input string and output string are same or your internet is unstable.")
+    exit()
+
+try:
+    tts = gTTS(text=str(trans_text), lang=str(out_lang))
+    tts.save("translation.mp3")
+    playsound("translation.mp3")
+except:
+    print("Error while performing text-to-speech")
+    exit()
