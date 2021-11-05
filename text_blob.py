2d6888715ab79871605c9559f8db7af07a87c97a
Atyant Sony <atyant20039@iiitd.ac.in>
2021-11-05 18:35:56 +0530

    Added functionality to speak out the translated string using gTTS and playsound python libraries

--- text_blob.py
+++ text_blob.py
@@ -6,14 +6,30 @@
 # Learn more about translate here:-
 # https://pypi.org/project/textblob/
 
+# Update:
+# Added functionality to speak out the translated output.
+# I have used gtts library for producing the audio and playsound library to play the produced audio
 
 from textblob import TextBlob
+from gtts import gTTS
+from playsound import playsound
 
 print("Enter Input String:")
-str = input()
+string = input()
 print("Enter Output language:")
 out_lang = input()
 
-adding_blob = TextBlob(str)
-translated_text = adding_blob.translate(to = out_lang)
-print(translated_text)
+try:
+    adding_blob = TextBlob(string)
+    translated_text = adding_blob.translate(to = out_lang)
+    print("Translated Text:")
+    print(translated_text)
+except:
+    print("Error while translating. Probably the input string and the translated string are same.")
+
+try:
+    tts = gTTS(text=str(translated_text), lang=str(out_lang))
+    tts.save("translation.mp3")
+    playsound("translation.mp3")
+except:
+    print("Error while performing text-to-speech")
